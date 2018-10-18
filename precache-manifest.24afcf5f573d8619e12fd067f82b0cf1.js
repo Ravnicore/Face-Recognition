66b3fdbe8c92fb0d637aa1cf8f041d9ff0b94229
Ravnicore <kevin.m.kelly@live.com>
2018-10-18 17:11:57 -0400

    Updates

---
+++ precache-manifest.24afcf5f573d8619e12fd067f82b0cf1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "bf1698323cb6ff2bd426",
+    "url": "/Face-API-Recognition/static/js/runtime~main.bf169832.js"
+  },
+  {
+    "revision": "e34a39167e5bbc3d0e0e",
+    "url": "/Face-API-Recognition/static/js/main.e34a3916.chunk.js"
+  },
+  {
+    "revision": "6bbd1a8a225f8879aa12",
+    "url": "/Face-API-Recognition/static/js/1.6bbd1a8a.chunk.js"
+  },
+  {
+    "revision": "e34a39167e5bbc3d0e0e",
+    "url": "/Face-API-Recognition/static/css/main.0d64d1ca.chunk.css"
+  },
+  {
+    "revision": "6bbd1a8a225f8879aa12",
+    "url": "/Face-API-Recognition/static/css/1.8e493c87.chunk.css"
+  },
+  {
+    "revision": "57f98d61f0b4494cbe6f4c19efc36c3d",
+    "url": "/Face-API-Recognition/index.html"
+  }
+];
